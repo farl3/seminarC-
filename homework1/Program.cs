68b5226695bacde23c034201bf2051959adcc941
farl3 <vd.av@ya.ru>
2022-10-16 17:01:16 +0300

    убрал лишнее

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -7,13 +7,11 @@ Console.WriteLine("первые max и min");
 int a = 5;
 int b = 7;
 
-int max = 0;
-int min = 10;
+int max = a;
+int min = b;
 
-if (a > max) max = a;
 if (b > max) max = b;
 if (min > a) min = a;
-if (min > b) min = b;
 
 Console.Write("max=");
 Console.WriteLine(max);
@@ -26,13 +24,11 @@ Console.WriteLine("вторые max и min");
 int a1 = -9;
 int b1 = -3;
 
-int max1 = -10;
-int min1 = 0;
+int max1 = a1;
+int min1 = b1;
 
-if (a1 > max1) max1 = a1;
 if (b1 > max1) max1 = b1;
 if (min1 > a1) min1 = a1;
-if (min1 > b1) min1 = b1;
 
 Console.Write("max=");
 Console.WriteLine(max1);
