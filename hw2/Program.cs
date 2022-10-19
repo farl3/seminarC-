3af12de0a906aacc702fb4d6581c8d964aa53f78
farl3 <vd.av@ya.ru>
2022-10-19 22:06:03 +0300

    задание2

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -3,35 +3,16 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-int a = 2;
-int b = 3;
-int c = 7;
-
-int max = a;
-
-if (b > max) max = b;
-if (c > max) max = c;
-Console.Write("max="); 
-Console.WriteLine(max); 
-
-int a1 = 44;
-int b1 = 5;
-int c1 = 78;
-
-int max1 = a1;
-
-if (b1 > max1) max1 = b1;
-if (c1 > max1) max1 = c1;
-Console.Write("max1="); 
-Console.WriteLine(max1); 
-
-int a2 = 22;
-int b2 = 3;
-int c2 = 9;
-
-int max2 = a2;
-
-if (b2 > max2) max2 = b2;
-if (c2 > max2) max2 = c2;
-Console.Write("max2="); 
-Console.WriteLine(max2); 
+int replay(string text)
+{Console.Write(text);
+int number=Convert.ToInt32(Console.ReadLine());
+return number;}
+int number1=replay("Введите первое число:");
+int number2=replay("Введите второе число:");
+int number3=replay("Введите третье число:");
+int max=number1;
+if(max<number2)
+{ max=number2;}
+if(max<number3)
+{max=number3;}
+Console.WriteLine($"{max}:Максимальное число из введённых");
