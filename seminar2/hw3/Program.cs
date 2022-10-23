6423cee6d627c28b85ee1ee307d3d531a69894ad
farl3 <vd.av@ya.ru>
2022-10-23 18:59:16 +0300

    семинар2, задание3

---
+++ seminar2/hw3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+int N = Prompt("Введите число: ");
+bool ValidateNumber(int N)
+{
+    if (N < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+if (ValidateNumber(N))
+{
+    while (N > 1000)
+    {
+        N = N / 10;
+    }
+    int A = N % 10;
+    System.Console.WriteLine($"Третья цифра числа: {N} равна {A}");
+}
