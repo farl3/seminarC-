81b636c78b05d6ae84cf41de3c7d4db89a083fae
farl3 <vd.av@ya.ru>
2022-10-23 19:55:25 +0300

    семинар2, задание4

---
+++ seminar2/hw4/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   
+    return int.Parse(Console.ReadLine());   
+}
+
+int N = Prompt("Введите число от 1 до 7, соответствующее дню недели: ");
+
+bool ValidateNumber(int N)
+{
+    if (N < 1 || N > 7)
+    {
+        System.Console.WriteLine("Введённое число не соответстует дню недели");
+        return false;
+    }
+    return true;
+}
+if (ValidateNumber(N))
+{
+    if (N >= 1 && N < 6)
+    {
+        System.Console.WriteLine($"{N} не является выходным днём");
+    }
+    else
+    {
+        System.Console.WriteLine($"{N} является выходным днём");
+    }
+   
+}
