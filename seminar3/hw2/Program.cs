e17bacef0c1db58e805f0663a992e01dc21714d2
farl3 <vd.av@ya.ru>
2022-10-23 20:43:36 +0300

    семинар3, задание3

---
+++ seminar3/hw2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задача 2: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+
+int x1 = Prompt("Введите x1:");
+int y1 = Prompt("Введите y1:");
+int z1 = Prompt("Введите z1:");
+
+int x2 = Prompt("Введите x2:");
+int y2 = Prompt("Введите y2:");
+int z2 = Prompt("Введите z2:");
+
+int x = x1 - x2;
+int y = y1 - y2;
+int z = z1 - z2;
+
+double Lenght = Math.Sqrt(x * x + y * y + z * z);
+Console.WriteLine($"{Lenght:f2}");
