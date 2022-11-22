b51cb331e630e6a4cfedde5a2253a59e5c34e63d
farl3 <vd.av@ya.ru>
2022-11-22 22:53:58 +0300

    семинар7 задание

---
+++ seminar7/hm2/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+(int rowNum, int colNum) PromptCoordinatesOfArray(string message) // Ввод координат через пробел
+{
+    System.Console.Write(message);
+    string temp = System.Console.ReadLine();
+    var dimensionValue = temp.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    int row = Convert.ToInt32(dimensionValue[0]);
+    int col = Convert.ToInt32(dimensionValue[1]);
+    return (row, col);
+}
+
+double GenerateRandomDouble(int min, int max, int accuracy) // Генерирует случайное вещественное число в заданных целых границах.
+{
+    double temp = new Random().Next(min, max + 1);
+    if (temp > 0) temp -= new Random().NextDouble();
+    else temp += new Random().NextDouble();
+    return Math.Round(temp, accuracy);
+}
+
+double[,] CreateRandomArrayOfDouble(int rows, int cols, int min, int max, int accuracy) // Создаем двумерный массив с заданой размерностью и границами генерации случайных вещественных чисел
+{
+    double[,] temp = new double[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            temp[i, j] = GenerateRandomDouble(min, max, accuracy);
+        }
+    }
+    return temp;
+}
+
+void PrintArrayOfDouble(double[,] array) // Печать двумерного массива вещественных чисел
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"{array[i, 0]}\t");
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+bool IsElementInArray(double[,] array, int row, int col) // Проверка по адресу наличия элемента в двумерном массиве (нумерация строк и столбцов начинается с 0)
+{
+    if (row >= 0 && col >= 0 && row < array.GetLength(0) && col < array.GetLength(1)) return true;
+    else return false;
+}
+
+void PrintElementOfArray(double[,] array, int row, int col) // Вывод элемента двумерного массива по адресу (нумерация строк и столбцов начинается с 0)
+{
+    if (IsElementInArray(array, row, col))
+    {
+        System.Console.WriteLine($"Значение элемент массива с адресом [{row + 1}, {col + 1}] равно {array[row, col]}");
+    }
+    else
+    {
+        System.Console.WriteLine($"В массиве нет элемента с адресом [{row + 1}, {col + 1}]");
+    }
+}
+
+void Execute() // Основное тело программы
+{
+    System.Console.Clear();
+    System.Console.WriteLine("Данная программа получает на вход координаты элемента массива и возвращает значение этого элемента.");
+
+    // === Блок с константами для удобного управления параметрами массива ===
+    const int rowsInArray = 5;
+    const int colsInArray = 8;
+    const int minOfRandom = -10;
+    const int maxOfRandom = 10;
+    const int accuracyOfRound = 2;
+    // === Конец блока констант ===
+
+    double[,] arrayOfDouble = CreateRandomArrayOfDouble(rowsInArray, 
+    colsInArray, 
+    minOfRandom, 
+    maxOfRandom, 
+    accuracyOfRound);
+    PrintArrayOfDouble(arrayOfDouble);
+
+    var dimensionsOfArray = PromptCoordinatesOfArray("Введите адрес интересующего вас элемента через пробел (нумерация начинается с 1). Например:5 2 -> ");
+    PrintElementOfArray(arrayOfDouble, dimensionsOfArray.rowNum - 1, dimensionsOfArray.colNum - 1);
+}
+
+Execute();
---
+++ seminar7/hm3/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 3. Задайте двумерный массив из целых чисел.
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintMsg(string message)   //выводим сообщение на экран
+{
+    Console.Write(message);
+}
+
+int GetUserInput(string message) //Принимает ввод от пользователя
+{
+    PrintMsg($"{message}:>");
+    string input = Console.ReadLine();
+    if (input == "") return 0;
+    return int.Parse(input);
+}
+
+int GetRandomInt(int minRandom, int maxRandom)  //возвращает вещественное число из диапазона [minRandom;maxRandom)
+{
+    return new Random().Next(minRandom, maxRandom);
+}
+
+int[,] CreateRandomMatrixInt(int rows, int columns, int minRandom = 0, int maxRandom = 10)   //возвращает случайную матрицу заданной размерности
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = GetRandomInt(minRandom, maxRandom);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)  //Выводит на экран содержимое матрицы
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.Write("\n");
+    }
+}
+
+void PrintMeanInColumns(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            sum += matrix[i, j];
+        Console.Write($"{sum / matrix.GetLength(0):f2}\t");
+    }
+}
+
+Console.Clear();
+PrintMsg("Программа находит среднее арифметическое элементов в каждом столбце\n");
+PrintMsg("двумерного массива случайных целых чисел.");
+int mSize = 5, nSize = 5;   //задаем размерность матрицы
+int lowRndLimit = 0, upRndLimit = 10; //задаем пределы случайных чисел
+int[,] matrix = CreateRandomMatrixInt(mSize, nSize, lowRndLimit, upRndLimit);
+PrintMsg("\n");
+PrintMatrix(matrix);
+PrintMsg("\n");
+PrintMeanInColumns(matrix);
+Console.WriteLine();
